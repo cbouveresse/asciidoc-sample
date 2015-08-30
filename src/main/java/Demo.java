62838f5bad0c1d76a0a24e7a2372463cd0d272b0
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2015-08-30 17:22:04 +0200

    Init Commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,24 @@
+
+/**
+ * <p>Demo.</p>
+ *
+ * @author anavarro - Apr 27, 2014
+ *
+ */
+public class Demo {
+
+    /**
+     * Constructor.
+     *
+     */
+    public Demo() {
+        // START SNIPPET: snip-id
+        
+        // TODO Auto-generated constructor stub
+
+
+        // END SNIPPET: snip-id
+    }
+    
+
+}
---
+++ src/main/java/asciidoc_maven_site_sample/asciidoc_maven_site_sample/App.java
@@ -0,0 +1,15 @@
+package asciidoc_maven_site_sample.asciidoc_maven_site_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    // tag::main[]
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello AsciiDoc!" );
+    }
+    // end::main[]
+}
